eda59091c986bdab7bb88cad3a07a0d84a603452
Pratik <pratikbhagwat4@gmail.com>
2025-02-15 13:02:43 -0800

    deploy commit 3

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/stack-line-app/', // Replace <REPOSITORY_NAME> with your GitHub repository name
-});
